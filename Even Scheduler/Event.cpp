5fdb80301f842a08bc7b520c217eafa45ac087b7
Sapple21 <139970739+Sapple21@users.noreply.github.com>
2025-02-22 16:51:04 +0800

    Delete Even Scheduler directory

--- Even Scheduler/Event.cpp
+++
@@ -1,6 +0,0 @@
-#include "Event.hpp"
-
-Event::Event() : eventID(""), name(""), date(""), time(""), organizer(""), isApproved(false) {}
-
-Event::Event(std::string id, std::string n, std::string d, std::string t, std::string org, bool approved)
-    : eventID(id), name(n), date(d), time(t), organizer(org), isApproved(approved) {}
